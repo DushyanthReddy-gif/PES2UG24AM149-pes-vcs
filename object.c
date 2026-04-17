96ad95b4977a2558ffb231e370688315c93ddcdf
DushyanthReddy-gif <dushyanthseelam@gmail.com>
2026-04-17 13:02:42 +0000

    Added object type mapping and header construction

--- object.c
+++ object.c
@@ -100,50 +100,11 @@ int object_exists(const ObjectID *id) {
 // Returns 0 on success, -1 on error.
 int object_write(ObjectType type, const void *data, size_t len,  ObjectID *id_out) {
   // TODO: Implement
+  const char *type_str = (type == OBJ_BLOB) ? "blob" : 
+                       (type == OBJ_TREE) ? "tree" : "commit";
+
   char header[64];
-  const char *type_str;
-
-  if (type == OBJ_BLOB) type_str = "blob"; 
-  else if (type == OBJ_TREE) type_str = "tree";
-  else if (type == OBJ_COMMIT) type_str = "commit";
-  else return -1;
-
-  sprintf(header, "%s %zu", type_str, len);
-
-  size_t header_len = strlen(header);
-  size_t total_size = header_len + 1 + len;
-  unsigned char *buffer = malloc(total_size);
-
-  memcpy(buffer, header, header_len);
-  buffer[header_len] = '\0';
-  memcpy(buffer + header_len + 1, data, len);
- 
-  unsigned char hash[32];
-  SHA256(buffer, total_size, hash);
-
-  char hex[65];
-  for (int i = 0; i < 32; i++) {
-      sprintf(hex + i * 2, "%02x", hash[i]);
-  }
-  hex[64] = '\0';
-  
-  char dir[256];
-  sprintf(dir, ".pes/objects/%.2s", hex);
-
-  mkdir(".pes", 0755);
-  mkdir(".pes/objects", 0755);
-  mkdir(dir, 0755);
-
-  char path[512];
-  sprintf(path, "%s/%s", dir, hex + 2);
-
-  FILE *f = fopen(path, "wb");
-  fwrite(buffer, 1, total_size, f);
-  fclose(f);
-  
-  free(buffer);
-  memcpy(id_out, hash, 32);
-  return 0;
+  int header_len = snprintf(header, sizeof(header), "%s %zu", type_str, len) + 1;
 }
 
 // Read an object from the store.
